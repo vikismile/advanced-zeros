9666f47cc8867c36a3b5128b85c5d613e343a13b
viktoria <vikaorabey@mail.ru>
2018-02-23 12:42:08 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+      let a = 2
+      let b = base
+      let ar = []
+      let power
+      let sum = 0
+      while (a <= base && a <= b){
+          if (base % a == 0){
+            ar = base;
+            base = Math.floor(base/a)
+            
+          } else {
+            a++
+          }
+      }
+      for (let i = 1;i <= 10000;i ++)
+      {
+          power = Math.pow(ar,i)    
+          if (number <= power){
+            a = i
+            break
+          }   
+      }
+      for (let i = a;i >= 1;i --)              
+      {
+        round = Math.floor(number / Math.pow(ar, i))
+          sum = sum + round          
+      }
+      return sum;
 }
